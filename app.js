9feff22dd0fb0b92f8bd5d470c69b0ef6254d438
Roy Baron <baron.roy@gmail.com>
2019-04-02 23:53:51 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const getPosts = () => {
+//   axios.get('http://localhost:3000/posts')
+//     .then((posts) => {
+//       return posts;
+//     })
+//     .then((postsJSON) => {
+//       console.log(postsJSON.data);
+//     });
+// };
+
+// getPosts();
+
+console.log('hello!');
